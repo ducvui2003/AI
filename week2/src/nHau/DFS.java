dd5b16f70085ac6ed924f96f34676912fa493309
Le Anh Duc <ducvui2003@gmail.com>
2023-10-11 11:43:59 +0700

    update dfs week 2

--- week2/src/nHau/DFS.java
+++ week2/src/nHau/DFS.java
@@ -1,32 +1,28 @@
 package nHau;
 
 
-public class  DFS {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class DFS {
     public Node dfsUsingStack(Node node, int n) {
-//        ArrayList<Node> result = new ArrayList<>();
-//        Stack<Node> stack = new Stack<>();
-//        if (start == end) {
-//            result.add(start);
-//            return result.toArray();
-//        }
-//        start.setVisited(true);
-//        stack.add(start);
-//        while (!stack.isEmpty()) {
-//            Node q = stack.pop();
-//            List<Node> adjective = q.getNeighbours();
-//            for (int i = adjective.size() - 1; i >= 0; i--) {
-//                Node currentNode = adjective.get(i);
-//                if (!currentNode.isVisited()) {
-//                    currentNode.setParent(q);
-//                    if (currentNode == end) {
-//                        return Utils.getINSTANCE().path(adjective.get(i), start.getName());
-//                    } else {
-//                        currentNode.setVisited(true);
-//                        stack.add(adjective.get(i));
-//                    }
-//                }
-//            }
-//        }
+        Stack<Node> stack = new Stack<>();
+        if (node.getState().size() == n) {
+            return node;
+        }
+        stack.add(node);
+        while (!stack.isEmpty()) {
+            Node q = stack.pop();
+            List<Node> adjective = q.getNeighbours();
+            if (adjective == null) {
+                return q;
+            }
+            for (int i = 0; i < adjective.size(); i++) {
+                stack.add(adjective.get(i));
+            }
+        }
         return null;
     }
 }
--- week2/src/nHau/Queen.java
+++ week2/src/nHau/Queen.java
@@ -15,6 +15,7 @@ public class Queen {
     public void dfs() {
         DFS dfs = new DFS();
         this.goal = dfs.dfsUsingStack(new Node(n), n);
+        System.out.println(goal);
     }
 
     public void bfs() {
@@ -25,15 +26,9 @@ public class Queen {
 
     public static void main(String[] args) {
         Queen q = new Queen(8);
+        System.out.println("BFS: ");
         q.bfs();
-//        q.dfs();
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        List<Integer> list2 = new ArrayList<>(list);
-//        list.remove((Integer) 1);
-//        System.out.println(list);
-//        System.out.println(list2);
+        System.out.println("DFS: ");
+        q.dfs();
     }
 }
